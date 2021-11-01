277befa31fc28df879371151a6db786c9c354547
Olga <baigarovy@gmail.com>
2021-11-01 12:15:27 +0300

    first commit

---
+++ lesson_1/script.js
@@ -0,0 +1,30 @@
+/*console.log('hello');
+const obj = {
+    name: 'Alex',
+    age: 25,
+    ismarried: false
+};
+
+console.log(obj.ismarried);
+console.log(obj['name']);
+*/
+
+//const result = confirm('Вы здесь?');
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+
+/*const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);*/
+
+const category = 'toys';
+console.log('https://someurl.com/' + category);
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Hello, ${user}`);
